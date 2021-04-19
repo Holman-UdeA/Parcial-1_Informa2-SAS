93daeccaeb880f57897149facb4567da9e0f8561
Holman-UdeA <62768029+Holman-UdeA@users.noreply.github.com>
2021-04-19 01:13:13 -0500

    Primer código.
    Código hecho por el profesor Jonathan durante la explicación pero con la función Pulso, la cual se encarga de enviar los datos ingresados por el SER entre niveles para así usar menos lineas de código. (Pensaré en como conectar un 4HC595 mediante otro, para así solo usar 3 pines digitales del arduino.)

---
+++ sizzling_fyyran_jaban1.ino
@@ -0,0 +1,67 @@
+int SER = 2;
+
+int SCLRK = 4; //Primer pulso
+
+int RCLK = 3; //Segundo pulso
+
+
+
+void setup()
+
+{
+  
+   pinMode  (SER, OUTPUT);
+  
+   pinMode (RCLK, OUTPUT);
+
+   pinMode(SCLRK, OUTPUT);
+  
+  
+   digitalWrite(SER, 1);
+ 
+  
+Pulso(SCLRK);
+
+   Pulso(RCLK);
+
+   delay(500);
+
+  
+ digitalWrite(SER, 0);
+
+  
+ Pulso(SCLRK);
+  Pulso(RCLK);
+
+   delay(500);
+  
+  digitalWrite(SER, 1);
+
+  
+ Pulso(SCLRK);
+
+   Pulso(RCLK);
+
+}
+
+
+
+void loop()
+
+{
+  
+
+}
+
+
+void Pulso(int NumPulso)
+
+{
+  
+   digitalWrite(NumPulso, 0);
+
+   digitalWrite(NumPulso, 1);
+
+   digitalWrite(NumPulso, 0);
+
+}
